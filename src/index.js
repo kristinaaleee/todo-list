cf07500155d6b7dbae0d588ea1cabc25d51273fe
Kristina Lee <kristinalee2000@hotmail.com>
2025-04-09 19:02:26 -0400

    Add style for cards on main panel

--- src/index.js
+++ src/index.js
@@ -64,10 +64,72 @@
 //     }
 // })
 
-noteTab.addEventListener('click', () => {
-    content.innerHTML = ''
-    for (let note in notesArray){
-        addNotes(notesArray[note]);
-        console.log(notesArray[note]);
-    }
-})
+// noteTab.addEventListener('click', () => {
+//     content.innerHTML = ''
+//     for (let note in notesArray){
+//         addNotes(notesArray[note]);
+//         console.log(notesArray[note]);
+//     }
+// })
+
+//LOCAL STORAGE TESTER FILE
+// class Item {
+//     constructor(title, description, date, priority, project, complete){
+//         this.title = title;
+//         this.description = description;
+//         this.date = date;
+//         this.priorty = priority;
+//         this.project = project;
+//         this.complete = complete;
+//     }
+//     completeToggle(){
+//         if (this.complete == false) {
+//             return this.complete = true
+//         }
+//         else {
+//             return this.complete = false
+//         }
+//     }
+// }
+
+
+//get, parse, read, print
+
+//add array to local storage everytime new component is added
+
+//.get array, .parse, push, .set(stringify)
+// function addItemLocal(item){
+//     localStorage.setItem(`${item.title}`, JSON.stringify(item))
+// }
+
+// function addProjectLocal(project){
+//     localStorage.setItem(`${project}`, project)
+// }
+
+// function addNotesLocal(note){
+//     localStorage.setItem(`${note}`, note)
+// }
+// // tab switch/form display
+// const formButton = document.getElementById('new');
+// const dialog = document.getElementById('dialog');
+// const tabContent = document.getElementsByClassName('tabcontent');
+// const tabButtons = document.getElementsByClassName("tabs");
+
+// function tabClear(){
+//     for (let i = 0; i < tabContent.length; i++) {
+//         tabContent[i].style.display = 'none';
+//     }
+// }
+
+// formButton.addEventListener('click', () =>{
+//     tabClear();
+//     tabContent[0].style.display = 'block';
+//     dialog.showModal();
+// })
+
+// for (let i = 0; i < tabContent.length; i++){
+//     tabButtons[i].addEventListener('click', () =>{
+//         tabClear();
+//         tabContent[i].style.display = 'block';
+//     })
+// }
--- src/logic.js
+++ src/logic.js
@@ -35,6 +35,7 @@ function addTodoItem(item){
     const infoWrapper = document.createElement('div')
     infoWrapper.classList.add('row')
     const cardDetail = document.createElement('div');
+    cardDetail.setAttribute('id', 'card-detail')
     const priorityBar = document.createElement('div');
     priorityBar.setAttribute('id','priority')
     switch(item.priority){
@@ -49,19 +50,20 @@ function addTodoItem(item){
             break;
     }
 
-    const title = document.createElement('h2');
+    const title = document.createElement('h3');
     title.textContent = `${item.title}`;
     const dueDate = document.createElement('p');
     dueDate.textContent = `${item.date}`;
 
     const buttonWrapper = document.createElement('div')
+    buttonWrapper.setAttribute('id', 'button-wrapper')
     const editButton = document.createElement('button');
     editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" /></svg>'
 
     const delButton = document.createElement('button');
     delButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>'
-    const completeButton = document.createElement('button'); //edit visual checkbox
-    completeButton.textContent = 'Done?'
+    const completeButton = document.createElement('input'); //edit visual checkbox
+    completeButton.setAttribute('type', 'checkbox')
 
     cardDetail.appendChild(title);
     cardDetail.appendChild(dueDate);
@@ -80,7 +82,7 @@ function addTodoItem(item){
     content.appendChild(card);
 
     delButton.addEventListener('click', function(){
-        delButton.parentElement.remove();
+        card.remove();
         const index = todoList.indexOf(item)
         todoList.splice(index,1)
         console.log(todoList) 
@@ -88,7 +90,6 @@ function addTodoItem(item){
 
     editButton.addEventListener('click', function(){
         updateForm(item);
-        editButton.parentElement.remove();
         // goal is to remove after
     })
 }
